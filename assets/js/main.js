ebb157e1d7df8384d08151903aabbdda60c6c49d
Max Ivanov <waxivanov@gmail.com>
2020-12-02 11:28:12 +0200

    upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,18 @@
-const burgerBtn = document.querySelector(".burger_btn");
+const burgerBtn = document.querySelector('.burger_btn');
+const burgerMenu = document.querySelector('.burger_menu');
+
 let isBurgerMenuOpen = false;
 
-burgerBtn.addEventListener("click", onBurgerBtnClick);
+burgerBtn.addEventListener('click', onBurgerBtnClick);
 
 function onBurgerBtnClick() {
   if (!isBurgerMenuOpen) {
-    burgerBtn.classList.add("active");
+    burgerBtn.classList.add('active');
+    burgerMenu.classList.add('active');
     isBurgerMenuOpen = true;
   } else {
-    burgerBtn.classList.remove("active");
+    burgerBtn.classList.remove('active');
+    burgerMenu.classList.remove('active');
     isBurgerMenuOpen = false;
   }
 }
