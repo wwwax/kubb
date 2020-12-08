9015573b259cbc8fc55cdc6703bc92571180b30c
Max Ivanov <waxivanov@gmail.com>
2020-12-08 15:02:40 +0200

    navigation done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 const burgerBtn = document.querySelector('.burger_btn');
-const menuMobile = document.querySelector('.menu-mobile');
-let isMenuMobileOpen = false;
+const menu = document.querySelector('.menu');
+let isMenuOpen = false;
 
 burgerBtn.addEventListener('click', onBurgerBtnClick);
 
 function onBurgerBtnClick() {
-  if (!isMenuMobileOpen) {
+  if (!isMenuOpen) {
     burgerBtn.classList.add('active');
-    menuMobile.classList.add('active');
-    isMenuMobileOpen = true;
+    menu.classList.add('active');
+    isMenuOpen = true;
   } else {
     burgerBtn.classList.remove('active');
-    menuMobile.classList.remove('active');
-    isMenuMobileOpen = false;
+    menu.classList.remove('active');
+    isMenuOpen = false;
   }
 }
