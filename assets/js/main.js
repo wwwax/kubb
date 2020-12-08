85c8cd90141a4514266c6e27ce5482ddb9be5cb1
Max Ivanov <waxivanov@gmail.com>
2020-12-08 09:42:38 +0200

    burger btn done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 const burgerBtn = document.querySelector('.burger_btn');
-const burgerMenu = document.querySelector('.burger_menu');
-
+// const burgerMenu = document.querySelector('.burger_menu');
 let isBurgerMenuOpen = false;
 
 burgerBtn.addEventListener('click', onBurgerBtnClick);
@@ -8,11 +7,11 @@ burgerBtn.addEventListener('click', onBurgerBtnClick);
 function onBurgerBtnClick() {
   if (!isBurgerMenuOpen) {
     burgerBtn.classList.add('active');
-    burgerMenu.classList.add('active');
+    // burgerMenu.classList.add('active');
     isBurgerMenuOpen = true;
   } else {
     burgerBtn.classList.remove('active');
-    burgerMenu.classList.remove('active');
+    // burgerMenu.classList.remove('active');
     isBurgerMenuOpen = false;
   }
 }
