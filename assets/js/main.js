4bf9fc45d06109a8e946675ee294749fd926eba9
Max Ivanov <waxivanov@gmail.com>
2020-12-09 13:05:57 +0200

    fix body overflow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const burgerBtn = document.querySelector('.burger_btn');
 const menu = document.querySelector('.menu');
+const body = document.querySelector('body');
 let isMenuOpen = false;
 
 burgerBtn.addEventListener('click', onBurgerBtnClick);
@@ -8,10 +9,12 @@ function onBurgerBtnClick() {
   if (!isMenuOpen) {
     burgerBtn.classList.add('active');
     menu.classList.add('active');
+    body.classList.add('lock');
     isMenuOpen = true;
   } else {
     burgerBtn.classList.remove('active');
     menu.classList.remove('active');
+    body.classList.remove('lock');
     isMenuOpen = false;
   }
 }
